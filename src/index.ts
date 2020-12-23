0f1d2de7c10de6976489797f93e12c6d004a81d8
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2020-12-23 17:58:56 -0500

    add curd actions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post";
@@ -6,6 +7,7 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   // connect to DB
@@ -13,18 +15,13 @@ const main = async () => {
   // run migrations
   await orm.getMigrator().up();
 
-  // const post = orm.em.create(Post, { title: "my first post" });
-  // await orm.em.persistAndFlush(post);
-
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
-
   const app = express();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
 
   apolloServer.applyMiddleware({ app });
