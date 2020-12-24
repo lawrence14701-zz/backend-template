5847e2c5230029f826f539fba04a882371e67bc0
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2020-12-24 17:58:24 -0500

    add session and redis

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 
 const main = async () => {
   // connect to DB
@@ -16,12 +19,34 @@ const main = async () => {
   await orm.getMigrator().up();
 
   const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid", //name of cookie
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true, //if enabled and the user is active it will reset cookie expiration
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years
+        httpOnly: true, //security: frontend JS code cannot accesss cookie
+        sameSite: "lax", // related to protecting csrf
+        secure: __prod__, // cookie only works in https when in production
+      },
+      secret: "dklsadjskdjladj",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
