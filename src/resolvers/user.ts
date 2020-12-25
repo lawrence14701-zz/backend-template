6d7405efa6ac2e1afa18676089e9242143b9d75d
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2020-12-24 19:17:23 -0500

    add session token on register

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -7,6 +7,7 @@ import {
   InputType,
   Mutation,
   ObjectType,
+  Query,
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
@@ -40,10 +41,19 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    // you are not logged in
+    if (!req.session.userId) {
+      return null;
+    }
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -83,6 +93,9 @@ export class UserResolver {
       }
       console.log("message: ", err.message);
     }
+    //store user id session
+    // sets cookie on the user and keeps them logged in
+    req.session.userId = user.id;
     return { user };
   }
 
