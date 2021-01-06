84f683cfca0b0fec5704230f3d1544f612210570
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2021-01-06 13:56:40 -0500

    conevert post resolver to typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import "reflect-metadata";
-import { MikroORM } from "@mikro-orm/core";
+import "reflect-metadata"; //typeorm needs this
 import { COOKIE_NAME, __prod__ } from "./constants";
-import mikroConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
@@ -12,12 +10,20 @@ import Redis from "ioredis";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import cors from "cors";
+import { createConnection } from "typeorm";
+import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 
 const main = async () => {
-  // connect to DB
-  const orm = await MikroORM.init(mikroConfig);
-  // run migrations
-  await orm.getMigrator().up();
+  const connection = await createConnection({
+    type: "postgres",
+    database: "taptab",
+    username: "postgres",
+    password: "postgres",
+    logging: true,
+    synchronize: true, //creates tables automatically wihtout running migration
+    entities: [Post, User],
+  });
 
   const app = express();
 
@@ -56,7 +62,7 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }) => ({ em: orm.em, req, res, redis }),
+    context: ({ req, res }) => ({ req, res, redis }),
   });
 
   apolloServer.applyMiddleware({
